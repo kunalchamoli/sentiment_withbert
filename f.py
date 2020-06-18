9952eef5f9495ff122e7cd15926f9abb0d982572
Kunal Chamoli <kunalchamoli98@gmail.com>
2020-06-18 15:47:09 +0530

    validation step

--- f.py
+++ f.py
@@ -24,9 +24,11 @@ sh.mkdir('logs')
 class IMDB_sentiment(pl.LightningModule):
     def __init__(self):
         super().__init__()
-    
+        self.model = transformers.BertForSequenceClassification.from_pretrained(FLAGS.model)
+        self.loss = th.nn.CrossEntropyLoss(reduction='none')
+
+
     def prepare_data(self):
-        train_ds = nlp.load_dataset('imdb', split=f'train[:{FLAGS.batch_size if FLAGS.debug else f"{FLAGS.percent}%"}]')
         tokenizer = transformers.BertTokenizer.from_pretrained(FLAGS.model)
 
         def __tokenizer(x):
@@ -35,20 +37,53 @@ class IMDB_sentiment(pl.LightningModule):
                                                  pad_to_max_length=True)
             return x
         
-        train_ds = train_ds.map(__tokenizer)
-        train_ds.set_format(type='torch', columns=['input_ids', 'label'])
-        self.train_ds = train_ds
+
+        def _prepare_ds(split):
+            ds = nlp.load_dataset('imdb', split=f'{split}[:{FLAGS.batch_size if FLAGS.debug else f"{FLAGS.percent}%"}]')
+            ds = ds.map(__tokenizer) 
+            ds.set_format(type='torch', columns=['input_ids', 'label'])
+            return ds
+
+        self.train_ds, self.test_ds = map(_prepare_ds, ('train', 'test'))
+        
+
+    def forward(self, input_ids):
+        mask = (input_ids != 0).float()
+        logits, = self.model(input_ids, mask)
+        return logits
         #import IPython ; IPython.embed(); exit(1)
 
-    def forward(self, batch):
-        pass
-
     def training_step(self, batch, batch_idx):
-        pass
+        import IPython ; IPython.embed(); exit(1)
+
+    def validation_step(self, batch, batch_idx):
+        logits = self.forward(batch['input_ids'])
+        loss = self.loss(logits, batch['label'])
+        acc = (logits.argmax(-1) == batch['label']).float()
+        return {'loss': loss, 'acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        loss = th.cat([o['loss'] for o in outputs], 0).mean()
+        acc = th.cat([o['acc'] for o in outputs], 0).mean()
+        out = {'val_loss': loss, 'val_acc': acc}
+        return {**out, 'log': out}
+
 
     def train_dataloader(self):
-        pass
+        return th.utils.data.DataLoader(
+                self.train_ds,
+                batch_size=FLAGS.batch_size,
+                drop_last=True,
+                shuffle=True,
+                )
 
+    def val_dataloader(self):
+        return th.utils.data.DataLoader(
+                self.test_ds,
+                batch_size=FLAGS.batch_size,
+                drop_last=False,
+                shuffle=False,
+                )
     def configure_optimizers(self):
         return th.optim.SGD(
             self.parameters(),
